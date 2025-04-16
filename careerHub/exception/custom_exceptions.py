f3cad314d821a6d23a80b7319547c6cfecd192b0
Ashish_is_Live <113754601+Ashish-is-Live@users.noreply.github.com>
2025-04-16 16:24:26 +0530

    Delete careerHub directory

--- careerHub/exception/custom_exceptions.py
+++
@@ -1,5 +0,0 @@
-class InvalidEmailException(Exception): pass
-class NegativeSalaryException(Exception): pass
-class ResumeFileException(Exception): pass
-class DeadlinePassedException(Exception): pass
-class DBConnectionException(Exception): pass
--- careerHub/util/db_property_util.py
+++
@@ -1,5 +0,0 @@
-def get_connection_string(file_name):
-    with open(file_name) as f:
-        for line in f:
-            if line.startswith("url"):
-                return line.strip().split("=", 1)[1]
